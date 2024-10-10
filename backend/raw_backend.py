54c979413ccf24a771826eb227bb78bab1c13901
danil_proskurin <danielpashtetov@gmail.com>
2024-10-10 18:14:37 +0400

    add raw sql methods

--- backend/raw_backend.py
+++ backend/raw_backend.py
@@ -15,19 +15,6 @@ def _add_cursor_wrapper(func):
 	return wrapper
 
 
-# Request example:
-# @_add_cursor_wrapper
-# def get_library_books(cur, library_id):
-#     cur.execute(
-#         '''
-#         SELECT id, library_id, title, author, status FROM book
-#         WHERE library_id = %s
-#         ''',
-#         (library_id,)
-#     )
-#     return cur.fetchall()
-
-
 class RawBackend(BaseBackend):
 
 	def __init__(self):
@@ -36,32 +23,99 @@ class RawBackend(BaseBackend):
 
 	@_add_cursor_wrapper
 	def _populate_default_categories(self, cur):
-		pass
+		cur.execute(
+			'''
+			CREATE TABLE IF NOT EXISTS category (
+				id SERIAL PRIMARY KEY,
+				name VARCHAR(255) NOT NULL UNIQUE
+			)
+			'''
+		)
+		cur.execute(
+			f'''
+			INSERT INTO category (id, name) VALUES
+			    (1, 'mp3'),
+			    (2, 'wav'),
+			    (3, 'ogg'),
+			    (4, 'flac'),
+			    (5, 'aac'),
+			    (6, 'wma'),
+			    (7, 'm4a'),
+			    (8, 'aiff'),
+			    (9, 'alac'),
+			    (10, 'pcm');
+            '''
+		)
 
 	@_add_cursor_wrapper
-	def get_sample(self, cur,  name):
-		pass
+	def get_sample(self, cur, name):
+		cur.execute(
+			'''
+			SELECT name, category_id FROM sample
+			WHERE name = %s
+			''',
+			(name,)
+		)
+		cur.fetchone()
 
 	@_add_cursor_wrapper
 	def get_samples_by_category(self, cur, category_id):
-		pass
+		cur.execute(
+			'''
+			SELECT name FROM sample
+			WHERE category_id = %s
+			''',
+			(category_id,)
+		)
+		cur.fetchall()
 
 	@_add_cursor_wrapper
 	def add_sample(self, cur, sample, category_name):
-		pass
+		cur.execute(
+			'''
+			INSERT INTO sample (name, category_id)
+			VALUES (%s, %s)
+			''',
+			(sample, category_name)
+		)
 
 	@_add_cursor_wrapper
 	def delete_sample(self, cur, sample_name):
-		pass
+		cur.execute(
+			'''
+			DELETE FROM sample
+			WHERE name = %s
+			''',
+			(sample_name,)
+		)
 
 	@_add_cursor_wrapper
 	def add_category(self, cur, category):
-		pass
+		cur.execute(
+			'''
+			INSERT INTO category (name)
+			VALUES (%s)
+			''',
+			(category,)
+		)
 
 	@_add_cursor_wrapper
 	def delete_category(self, cur, category_id):
-		pass
+		cur.execute(
+			'''
+			DELETE FROM category
+			WHERE id = %s
+			''',
+			(category_id,)
+		)
 
 	@_add_cursor_wrapper
 	def get_category_id(self, cur, category_name):
-		pass
+		cur.execute(
+			'''
+			SELECT id FROM category
+			WHERE name = %s
+			''',
+			(category_name,)
+		)
+		cur.fetchone()
